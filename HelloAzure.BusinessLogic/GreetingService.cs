22ee6bc924e461b0bf3c659937bd1b26f5f71c05
Paddy Pillai <Paddy.Pillai@synechron.com>
2020-05-13 08:50:41 -0400

    inital code

---
+++ HelloAzure.BusinessLogic/GreetingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloAzure.BusinessLogic
+{
+    public class GreetingService
+    {
+        public string GetGreeting(string name)
+        {
+            return $"Hello, {name}";
+        }
+    }
+}
